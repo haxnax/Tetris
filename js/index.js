07c0bd185a56726f2228f04d4c01fe6e63e6e2d4
Myyar Hasan <myyar2012@gmail.com>
2026-05-12 22:43:48 +0200

    display grid

--- js/index.js
+++ js/index.js
@@ -142,6 +142,32 @@ function rotate(){
     draw()
 }
 
+const displaySquares= document.querySelectorAll('mini-grid div')
+const displayWidth = 4;
+let displayIndex=0;
+
+const upNextTetro = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2],
+    [0,displayWidth,displayWidth+1,displayWidth*2+1],
+    [1,displayWidth,displayWidth+1,displayWidth+2],
+    [0,1,displayWidth,displayWidth+1],
+    [1,displayWidth+1,displayWidth*2+1,displayWidth*3+1]
+];
+
+function
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 })
